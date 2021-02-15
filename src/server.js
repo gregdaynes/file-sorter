e52b098f20d3cb8f36152fb5ce77c4411753166a
Gregory Daynes <greg.daynes@pixelunion.net>
2021-02-15 11:39:59 -0800

    feat: /tags endpoint

--- src/server.js
+++ src/server.js
@@ -2,4 +2,12 @@ const log = require('lib/log')
 const app = require('express')()
 const { PORT } = require('lib/env')
 
+const dao = require('lib/dao')()
+const tagRepo = require('lib/tag-repo')(dao)
+
+app.get('/tags', async (req, res) => {
+  const tags = await tagRepo.getAll()
+
+  res.json({ data: { tags } })
+})
 app.listen(PORT, log.info.bind(null, `Running on port ${PORT}`))
